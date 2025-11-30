66907c9782d4641aa8e8959a1455c99203e961a2
m.ayoub <m.ayoub@sellanycar.com>
2025-11-30 17:22:32 +0400

    Test

--- app/(authenticated)/dashboard/studio/page.tsx
+++ app/(authenticated)/dashboard/studio/page.tsx
@@ -471,9 +471,9 @@ export default function StudioPage() {
                           {[
                             // { id: 'transparent', name: 'Transparent' },
                             { id: 'bg-1', name: 'Background 1' },
-                            { id: 'bg-7', name: 'Background 2' },
                             // { id: 'bg-8', name: 'Background 2' },
-                            { id: 'bg-2', name: 'Background 3' }
+                            { id: 'bg-2', name: 'Background 2' },
+                            { id: 'bg-3', name: 'Background 3' },
                           ].map((template) => (
                             <div
                               key={template.id}
